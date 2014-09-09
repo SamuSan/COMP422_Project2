60e82f4b0d23d67e47a9a7ccabee38473cf8d4af
SamuSan <sambminns@gmail.com>
2014-09-09 14:09:54 +1200

    Datasets converted to arff

--- COMP422_Proj2_P1/src/main/Main.java
+++ COMP422_Proj2_P1/src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 public class Main
 {
-
+private String  []  tasks = {"00", "15", "30", "60"};
     public static void main( String[] args )
     {
         // TODO Auto-generated method stub
