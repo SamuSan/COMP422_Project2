0a23ffd7485bfded995b17096ba241e99ca80192
SamuSan <sambminns@gmail.com>
2014-09-09 15:01:17 +1200

    Working through NN classifier

--- COMP422_Proj2_P1/src/main/Main.java
+++ COMP422_Proj2_P1/src/main/Main.java
@@ -1,12 +1,49 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import weka.classifiers.lazy.IBk;
+import weka.core.Instances;
+import weka.core.neighboursearch.NearestNeighbourSearch;
+
 public class Main
 {
-private String  []  tasks = {"00", "15", "30", "60"};
+    private static final String[] tasks = { "data/digits_00", "data/digits_15", "data/digits_30", "data/digits_60" };
+
     public static void main( String[] args )
     {
-        // TODO Auto-generated method stub
+        Instances train = null;
+        try
+        {
+            train = new Instances(
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    ClassLoader.getSystemResourceAsStream( tasks[0] ) ) ) );
+            train.setClassIndex( train.numAttributes() - 1 );
+            
+            IBk nn = new IBk();
+            
+            nn.buildClassifier( train );
+            System.out.println( train.numAttributes() );
+            System.out.println( train.classIndex() );
+        }
+        catch ( IOException e1 )
+        {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        catch ( Exception e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
+
+
+
+        System.out.println( "Done" );
     }
 
 }
